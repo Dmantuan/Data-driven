309f73c1b53950b6a1c27846ef1c354ef5f00974
Dmantuan <daniel2mantuan@gmail.com>
2023-11-03 15:08:24 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'data-driven',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
